82dc224635545dbcbe8f7bdc0020bbe8eedd09a9
xwwwb <xuwenbo@88.com>
2022-06-18 23:59:17 +0800

    完成动态数组

--- 顺序表/动态数组/MyArray.h
+++ 顺序表/动态数组/MyArray.h
@@ -25,13 +25,32 @@ public:
   // 拷贝构造
   MyArray(const MyArray &a);
   // 成员函数
-  void push_back(T data);
-  void push_front(T data);
+  void push_back(const T &data);
+  void push_front(const T &data);
+  // 插入到下标为pos的元素之后
+  void insertByPos(const T &data, size_t pos);
+  // 插入到数据为findData的元素之后
+  void insertByData(const T &insertData, const T &findData);
   void travel();
 
 private:
   void _clear();
+
+  // 从容器中找数据为findData的元素
+  // 找到了返回其下标 没找到返回-1
+  int _findData(const T &findData);
 };
+
+template <class T>
+int MyArray<T>::_findData(const T &findData)
+{
+  for (int i = 0; i < size; i++)
+  {
+    if (findData == pArr[i])
+      return i;
+  }
+  return -1;
+}
 
 template <class T>
 void MyArray<T>::_clear()
@@ -57,7 +76,7 @@ MyArray<T>::MyArray(const MyArray<T> &a)
 }
 
 template <class T>
-void MyArray<T>::push_back(T data)
+void MyArray<T>::push_back(const T &data)
 {
   // 判断是否需要开空间
   if (size >= capacity)
@@ -82,7 +101,7 @@ void MyArray<T>::push_back(T data)
 }
 
 template <class T>
-void MyArray<T>::push_front(T data)
+void MyArray<T>::push_front(const T &data)
 {
   if (size >= capacity)
   {
@@ -129,4 +148,94 @@ void MyArray<T>::travel()
     cout << pArr[i] << " ";
   }
   cout << endl;
+}
+
+template <class T>
+void MyArray<T>::insertByPos(const T &insertData, size_t pos)
+{
+  int ret = pos;
+  if (ret == -1)
+  {
+    push_back(insertData);
+  }
+  else if (ret == 0)
+  {
+    push_front(insertData);
+  }
+  else
+  {
+    //插入到下标为pos的元素之后
+    if (size >= capacity)
+    {
+      capacity = capacity +
+                 (((capacity >> 1) > 1)
+                      ? (capacity >> 1)
+                      : 1);
+      T *pNew = new T[capacity];
+      if (pArr)
+      {
+        memcpy(pNew, pArr, sizeof(T) * size);
+        delete[] pArr;
+      }
+      pArr = pNew;
+    }
+    for (int i = size - 1; i >= ret; i--)
+    {
+      pArr[i + 1] = pArr[i];
+    }
+    pArr[ret + 1] = insertData;
+    size++;
+  }
+}
+
+template <class T>
+void MyArray<T>::insertByData(const T &insertData, const T &findData)
+{
+  // 1. 找数据 可能存在找不到的情况
+  int ret = _findData(findData);
+  if (-1 == ret)
+  {
+    this->push_back(insertData);
+  }
+  else if (0 == ret)
+  {
+    push_front(insertData);
+  }
+  else
+  {
+    if (size >= capacity)
+    {
+      capacity = capacity +
+                 (((capacity >> 1) > 1)
+                      ? (capacity >> 1)
+                      : 1);
+      T *pNew = new T[capacity];
+      if (pArr)
+      {
+        memcpy(pNew, pArr, sizeof(T) * size);
+        delete[] pArr;
+      }
+      pArr = pNew;
+    }
+
+    // 原 1 2 3 4 5 6
+    // ret = 1
+    // i>=ret
+    //
+    // 1 2 2 3 3 4 5
+    // i >ret
+    // 1 2 3 3 3 4 5
+    // 中间差了一个
+    // pArr[2]=pArr[1]
+    // 但是这个位置搬不搬都会被pArr[ret+1]覆盖
+    // 所以不搬更好 推荐用 > 而不是 >=
+    for (int i = size - 1; i > ret; i--)
+    {
+      pArr[i + 1] = pArr[i];
+    }
+    pArr[ret + 1] = insertData;
+    // size这里用于遍历和比较capacity 放在这里或者放前面都行
+    // 放后面的话 在前面调用travel size会少一个
+    size++;
+  }
 }
--- 顺序表/动态数组/main.cpp
+++ 顺序表/动态数组/main.cpp
@@ -3,22 +3,39 @@
 int main()
 {
   MyArray<int> arr;
-  MyArray<double> darr;
-  MyArray<string> sarr;
-  for (int i = 0; i < 10; i++)
-  {
-    arr.push_back(i);
-    arr.travel();
-  }
-  for (int i = 0; i < 10; i++)
-  {
-    darr.push_back(i + 0.1);
-    darr.travel();
-  }
-  for (int i = 0; i < 10; i++)
-  {
-    sarr.push_front(to_string(i));
-    sarr.travel();
-  }
+  // MyArray<double> darr;
+  // MyArray<string> sarr;
+  // for (int i = 0; i < 10; i++)
+  // {
+  //   arr.push_back(i);
+  //   arr.travel();
+  // }
+  // for (int i = 0; i < 10; i++)
+  // {
+  //   darr.push_back(i + 0.1);
+  //   darr.travel();
+  // }
+  // for (int i = 0; i < 10; i++)
+  // {
+  //   sarr.push_front(to_string(i));
+  //   sarr.travel();
+  // }
+
+  arr.push_back(1);
+  arr.push_back(2);
+  arr.push_back(3);
+  arr.push_back(4);
+  arr.push_back(5);
+  arr.push_back(6);
+  arr.insertByPos(3, 3);
+  arr.travel();
+  arr.insertByData(9, 2);
+  arr.travel();
+  arr.insertByData(0, 1);
+  arr.travel();
+  arr.insertByPos(0, 0);
+  arr.travel();
+  arr.insertByPos(4, 2);
+  arr.travel();
   return 0;
 }
